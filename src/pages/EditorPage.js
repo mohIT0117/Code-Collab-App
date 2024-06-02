9f728c833fd909772c8b5ab14b1f5e08a061ba65
Mohit_Tiwari <mstiwari7532@gmail.com>
2024-06-02 16:16:47 +0530

    Initial Commit

---
+++ src/pages/EditorPage.js
@@ -0,0 +1,288 @@
+import React, { useState, useRef, useEffect } from "react";
+import ACTIONS from "../Actions";
+import Client from "../components/Client";
+import Editor from "../components/Editor";
+import { initSocket } from "../socket";
+import {
+  Navigate,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
+import { toast } from "react-hot-toast";
+import axios from "axios";
+
+const EditorPage = () => {
+  const socketRef = useRef(null);
+  const codeRef = useRef(null);
+  const location = useLocation();
+  const reactNavigator = useNavigate();
+  const { roomId } = useParams();
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    const init = async () => {
+      socketRef.current = await initSocket();
+
+      socketRef.current.on("connect_error", (err) => handleErrors(err));
+      socketRef.current.on("connect_failed", (err) => handleErrors(err));
+
+      function handleErrors(e) {
+        console.log("socket error", e);
+        toast.error("Socket connection failed, try again later.");
+        reactNavigator("/");
+      }
+
+      socketRef.current.emit(ACTIONS.JOIN, {
+        roomId,
+        username: location.state?.username,
+      });
+
+      //Listening for joined event
+      socketRef.current.on(
+        ACTIONS.JOINED,
+        ({ clients, username, socketId }) => {
+          if (username !== location.state?.username) {
+            toast.success(`${username} joined the room.`);
+            console.log(`${username} joined`);
+          }
+          setClients(clients);
+          socketRef.current.emit(ACTIONS.SYNC_CODE, {
+            code: codeRef.current,
+            socketId,
+          });
+        }
+      );
+
+      //Listeing for disconnected
+      socketRef.current.on(ACTIONS.DISCONNECTED, ({ socketId, username }) => {
+        toast.success(`${username} left the room.`);
+        setClients((prev) => {
+          return prev.filter((client) => client.socketId !== socketId);
+        });
+      });
+
+      //Listening for message
+      socketRef.current.on(ACTIONS.SEND_MESSAGE, ({ message }) => {
+        const chatWindow = document.getElementById("chatWindow");
+        var currText = chatWindow.value;
+        currText += message;
+        chatWindow.value = currText;
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+      });
+    };
+    init();
+    return () => {
+      socketRef.current.off(ACTIONS.JOINED);
+      socketRef.current.off(ACTIONS.DISCONNECTED);
+      socketRef.current.off(ACTIONS.SEND_MESSAGE);
+      socketRef.current.disconnect();
+    };
+  }, []);
+
+  async function copyRoomId() {
+    try {
+      await navigator.clipboard.writeText(roomId);
+      toast.success("Room ID has been copied to your clipboard");
+    } catch (err) {
+      toast.error("Could not copy the room id");
+      console.error(err);
+    }
+  }
+
+  function leaveRoom() {
+    reactNavigator("/");
+  }
+
+  if (!location.state) {
+    return <Navigate to="/" />;
+  }
+
+  const inputClicked = () => {
+    const inputArea = document.getElementById("input");
+    inputArea.placeholder = "Enter your input here";
+    inputArea.value = "";
+    inputArea.disabled = false;
+    const inputLabel = document.getElementById("inputLabel");
+    const outputLabel = document.getElementById("outputLabel");
+    inputLabel.classList.remove("notClickedLabel");
+    inputLabel.classList.add("clickedLabel");
+    outputLabel.classList.remove("clickedLabel");
+    outputLabel.classList.add("notClickedLabel");
+  };
+
+  const outputClicked = () => {
+    const inputArea = document.getElementById("input");
+    inputArea.placeholder =
+      "You output will apear here, Click 'Run code' to see it";
+    inputArea.value = "";
+    inputArea.disabled = true;
+    const inputLabel = document.getElementById("inputLabel");
+    const outputLabel = document.getElementById("outputLabel");
+    inputLabel.classList.remove("clickedLabel");
+    inputLabel.classList.add("notClickedLabel");
+    outputLabel.classList.remove("notClickedLabel");
+    outputLabel.classList.add("clickedLabel");
+  };
+
+  const runCode = () => {
+    const lang = document.getElementById("languageOptions").value;
+    const input = document.getElementById("input").value;
+    const code = codeRef.current;
+
+    toast.loading("Running Code....");
+
+    
+
+    const options = {
+      method: "POST",
+      url: "https://online-code-compiler.p.rapidapi.com/v1/",
+      headers: {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "99f06a4a19msha17cbe3ab09c13bp1ea9a8jsn88c8acbd9493",
+        "X-RapidAPI-Host": "online-code-compiler.p.rapidapi.com",
+      },
+      data: {
+    language: lang,
+    version: 'latest',
+    code: code,
+    input: input
+  },
+    };
+
+    
+
+    axios
+      .request(options)
+      .then(function (response) {
+        let message = response.data.output;
+        console.log(message);
+        if (message === null) {
+          message = response.data.Errors;
+        }
+        outputClicked();
+        document.getElementById("input").value = message;
+        toast.dismiss();
+        toast.success("Code compilation complete");
+      })
+      .catch(function (error) {
+        toast.dismiss();
+        toast.error("Code compilation unsuccessful");
+        document.getElementById("input").value =
+          "Something went wrong, Please check your code and input.";
+      });
+  };
+
+  const sendMessage = () => {
+    if (document.getElementById("inputBox").value === "") return;
+    var message = `> ${location.state.username}:\n${
+      document.getElementById("inputBox").value
+    }\n`;
+    const chatWindow = document.getElementById("chatWindow");
+    var currText = chatWindow.value;
+    currText += message;
+    chatWindow.value = currText;
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+    document.getElementById("inputBox").value = "";
+    socketRef.current.emit(ACTIONS.SEND_MESSAGE, { roomId, message });
+  };
+
+  const handleInputEnter = (key) => {
+    if (key.code === "Enter") {
+      sendMessage();
+    }
+  };
+
+  return (
+    <div className="mainWrap">
+      <div className="asideWrap">
+        <div className="asideInner">
+          <div className="logo">
+            <img className="logoImage" src="/code-collab+.png" alt="logo" />
+          </div>
+          <h3>Connected</h3>
+          <div className="clientsList">
+            {clients.map((client) => (
+              <Client key={client.socketId} username={client.username} />
+            ))}
+          </div>
+        </div>
+        <label>
+          Select Language:
+          <select id="languageOptions" className="seLang" defaultValue="python3">
+            <option value="csharp">C#</option>
+            <option value="java">Java</option>
+            <option value="python3">Python</option>
+            <option value="c">C</option>
+            <option value="cpp17">C++ (gcc)</option>
+            <option value="php">PHP</option>
+            <option value="nodejs">NodeJS</option>
+          </select>
+        </label>
+        <button className="btn runBtn" onClick={runCode}>
+          Run Code
+        </button>
+        <button className="btn copyBtn" onClick={copyRoomId}>
+          Copy ROOM ID
+        </button>
+        <button className="btn leaveBtn" onClick={leaveRoom}>
+          Leave
+        </button>
+      </div>
+
+      <div className="editorWrap">
+        <Editor
+          socketRef={socketRef}
+          roomId={roomId}
+          onCodeChange={(code) => {
+            codeRef.current = code;
+          }}
+        />
+        <div className="IO-container">
+          <label
+            id="inputLabel"
+            className="clickedLabel"
+            onClick={inputClicked}
+          >
+            Input
+          </label>
+          <label
+            id="outputLabel"
+            className="notClickedLabel"
+            onClick={outputClicked}
+          >
+            Output
+          </label>
+        </div>
+        <textarea
+          id="input"
+          className="inputArea textarea-style"
+          placeholder="Enter your input here"
+        ></textarea>
+      </div>
+
+      <div className="chatWrap">
+        <textarea
+          id="chatWindow"
+          className="chatArea textarea-style"
+          placeholder="Chat messages will appear here"
+          disabled
+        ></textarea>
+        <div className="sendChatWrap">
+          <input
+            id="inputBox"
+            type="text"
+            placeholder="Type your message here"
+            className="inputField"
+            onKeyUp={handleInputEnter}
+          ></input>
+          <button className="btn sendBtn" onClick={sendMessage}>
+            Send
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditorPage;
